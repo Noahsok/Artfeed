711b948a1047a282f59498d87dd58956be3baabd
Noahsok <NoahSokoloffinfo@gmail.com>
2018-12-13 14:42:12 -0500

    modified and removed: rm bootstrap2, madified frontend design

--- drum_artnews/settings.py
+++ drum_artnews/settings.py
@@ -248,6 +248,7 @@ INSTALLED_APPS = (
     "django.contrib.sitemaps",
     "django.contrib.staticfiles",
     "drum.links",
+    "bootstrap4",
     "mezzanine.boot",
     "mezzanine.conf",
     "mezzanine.core",
